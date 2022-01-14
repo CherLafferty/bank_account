cd005569142b0211ec098ea91386ef1e8e7a914f
Cher Lafferty <cherlafferty01@gmail.com>
2022-01-14 15:44:18 -0800

    third

--- bank_account.py
+++ bank_account.py
@@ -1,34 +1,42 @@
 class BankAccount:
     accounts = []
-    # don't forget to add some default values for these parameters!
+    #set default variables for int_rate and balance
     def __init__(self, int_rate = 0.01, balance = 0): 
-    # your code here! (remember, instance attributes go here)
+    # your code here-setting instance attributes
         self.int_rate = int_rate
         self.balance = balance
         BankAccount.accounts.append(self)
 
-    # don't worry about user info here; we'll involve the User class soon
+    # create deposit function
     def deposit(self, amount):
         self.balance += amount
         return self
 
+    # create withdraw function with conditional in event no funds available
     def withdraw(self, amount):
         if(self.balance - amount) >= 0:
             self.balance -= amount
         else:
+            #if funds below 0 will print insufficient funds and chrg fee statement
             print("Insufficient Funds: Chargin a $5 fee")
+            # deduct $5 fee from account
             self.balance -= 5
-        return self
+        return self 
 
+    # create display account info function with string
     def display_account_info(self):
-        print(f"Balance {self.balance}")
+        print(f"Balance: {self.balance}")
         return self
 
+    # crate interest yeild function
     def yield_interest(self):
         if(self.balance) > 0:
             self.balance += (self.balance * self.int_rate)
         return self
     
+    """create class method to display all accoutns
+    note: class methods have no access to the instance attribute
+    or any attribute that starts with self."""
     @classmethod
     def print_all_accounts(cls):
         for account in cls.accounts:
